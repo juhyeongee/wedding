cd00200489c1b92cf711496167413a18a8de9f50
juhyeong <juhyeongee@gmail.com>
2022-01-18 12:11:52 +0900

    test

---
+++ wedding/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import firstPic from "./images/IMG_2721.jpg";
+import widePic from "./images/IMG_2720.jpg";
+import secondPic from "./images/IMG_2722.jpg";
+import style from "./styles.css";
+
+function App() {
+  const [scrollY, setScrollY] = useState(0);
+  const myScroll = () => {
+    setScrollY(window.pageYOffset);
+  };
+  //useEffect(() => console.log(scrollY), [scrollY]);
+  useEffect(() => {
+    const whereIsMouse = () => {
+      window.addEventListener("scroll", myScroll);
+    };
+    whereIsMouse();
+    return window.removeEventListener("scroll", myScroll);
+    //오우..  리턴이었네..
+  });
+  /* const Picture1 = () => (
+    <div>
+      <img src={firstPic} width="390px" height="780px" />
+    </div>
+  );
+  //const Picture2 = () => <img src={secondPic} width="390px" height="780px" />;
+  const Picture3 = () => <img src={widePic} width="280px" height="200px" />;
+  //const Picture2 = new Picture(secondPic);
+*/
+  function Picture(props) {
+    return (
+      <div style={style}>
+        <img src={props.picnum} width="390px" height="780px" />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div style={style}> </div>
+      <div style={{ position: "fixed", top: "200px" }}>{scrollY}</div>
+      <Picture picnum={firstPic} />
+      <Picture picnum={secondPic} />
+      <Picture picnum={widePic} />
+    </>
+  );
+}
+
+export default App;
