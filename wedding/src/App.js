7e81b4bf102ea3f947e58cac1ee91ba581ce4fa8
juhyeong <juhyeongee@gmail.com>
2022-02-14 21:48:54 +0900

    2.14 Guest Book updating...

--- wedding/src/App.js
+++ wedding/src/App.js
@@ -19,10 +19,75 @@ function App() {
     };
     whereIsMouse();
     console.log(scrollY);
-    return window.removeEventListener("scroll", myScroll);
+    //return window.removeEventListener("scroll", myScroll);
   });
 
-  let POSITION_VALUE = (1000 / 2 - scrollY) / 1000;
+  let POSITION_VALUE = (1100 / 2 - scrollY) / 1000;
+  /////
+  ///// Time to make Guest Books
+  /////
+
+  const GuestBook_section = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    background-color: white;
+    width: 100%;
+    height: 50vh;
+    color: black;
+    font-size: 2rem;
+  `;
+
+  const Title = styled.text`
+    margin: 1rem;
+  `;
+
+  const MemoInput = styled.input`
+    height: 20vh;
+    width: 15vh;
+    background-color: cornsilk;
+    border: 1px solid black;
+  `;
+
+  const SubmitButton = styled.input``;
+
+  const handleTextSubmit = (event) => {
+    event.preventDefault();
+    const copyText = SubmitButton.value;
+  };
+
+  const Memoform = () => {
+    return (
+      <form>
+        <MemoInput type="text" placeholder="leave your message" />
+      </form>
+    );
+  };
+
+  const Memos = styled.div`
+    display: flex;
+  `;
+
+  const GuestBook = () => {
+    const [memo, setMemo] = useState([]);
+    //const newMemo =
+    //setMemo([...newMemo])
+    return (
+      <GuestBook_section>
+        <Title>Guest Book</Title>
+        <Memos>
+          <Memoform />
+          <Memoform />
+          <Memoform />
+          <Memoform />
+        </Memos>
+      </GuestBook_section>
+    );
+  };
+
+  ///// 포토북 위에
+  ///// 지도 (찾아오시는 길)
 
   return (
     <>
@@ -30,8 +95,9 @@ function App() {
       <div style={{ position: "fixed", top: "200px" }}>{scrollY}</div>
       <Picture picnum={firstPic} positionValue={POSITION_VALUE} />
       <Picture picnum={secondPic} positionValue={-POSITION_VALUE} />
-      <Picture picnum={widePic} />
+      {/*<Picture picnum={widePic} />*/}
       <AccountSection />
+      <GuestBook />
     </>
   );
 }
