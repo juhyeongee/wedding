6c46fe3a0eae295dfe056c48bd5201b2b5586926
juhyeong <juhyeongee@gmail.com>
2022-02-19 12:39:16 +0900

    adding firebase

---
+++ wedding/src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDCT4LN9t757TB4szGjumLWu57DTqMYipo",
+  authDomain: "weddingbjmj.firebaseapp.com",
+  projectId: "weddingbjmj",
+  storageBucket: "weddingbjmj.appspot.com",
+  messagingSenderId: "248446182610",
+  appId: "1:248446182610:web:9b2bc344e368bffda1790b",
+  measurementId: "G-SNYK41YDHC",
+};
+
+export default firebase.initializeApp(firebaseConfig);
